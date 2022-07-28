ce863315a68964cbb8a600cb16f5f1093d5427f1
vishnusomank <vishnu@accuknox.com>
2022-07-28 21:06:45 +0530

    database structure

--- models/db.go
+++ models/db.go
@@ -1,21 +1,9 @@
 package models
 
-type SBOM struct {
-	ID            uint   `json:"id" gorm:"primary_key"`
-	ImageName     string `json:"image"`
-	Version       string `json:"version"`
-	Value         string `json:"sbom"`
-	Vulnerability string `json:"vulnerability"`
-}
-
-type PolicyDB struct {
-	ID         uint   `json:"id" gorm:"primary_key"`
-	CVEId      string `json:"cve"`
-	PolicyData string `json:"policy"`
-	SBOMID     int    `json:"sbomID"`
-}
-
-type BinaryPathDB struct {
-	BinaryName string `json:"name"`
-	BinaryPath string `json:"path"`
+type XploitDB struct {
+	ID     uint   `json:"id" gorm:"primary_key"`
+	Title  string `json:"title"`
+	URL    string `json:"url"`
+	CVE    string `json:"cve" gorm:"unique"`
+	Author string `json:"author"`
 }
