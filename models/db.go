3b69acce64fb02d2fcf9bc2f9487e1156177363f
vishnusomank <vishnu@accuknox.com>
2022-07-29 15:19:35 +0530

    updated db structure

--- models/db.go
+++ models/db.go
@@ -1,9 +1,12 @@
 package models
 
 type XploitDB struct {
-	ID     uint   `json:"id" gorm:"primary_key"`
-	Title  string `json:"title"`
-	URL    string `json:"url" gorm:"unique"`
-	CVE    string `json:"cve"`
-	Author string `json:"author"`
+	ID       uint   `json:"id" gorm:"primary_key"`
+	Title    string `json:"title"`
+	URL      string `json:"url" gorm:"unique"`
+	CVE      string `json:"cve"`
+	Author   string `json:"author"`
+	Platform string `json:"platform"`
+	Type     string `json:"type"`
+	EDBID    string `json:"edb_id"`
 }
